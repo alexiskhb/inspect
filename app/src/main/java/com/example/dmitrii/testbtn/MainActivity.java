93ba21bf153882679dc9eeeb837143b9641ee3a5
DiMonster07 <territorycech@gmail.com>
2015-11-07 13:41:40 +1000

    Google Map

--- app/src/main/java/com/example/dmitrii/testbtn/MainActivity.java
+++ app/src/main/java/com/example/dmitrii/testbtn/MainActivity.java
@@ -1,40 +1,28 @@
 package com.example.dmitrii.testbtn;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    //private TextView HelloTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //HelloTextView = (TextView)findViewById(R.id.textViewid);
     }
 
     public void ClickBtn(View view) {
-        //HelloTextView.setText("Hello Kitty!");
         Intent intent_map = new Intent(MainActivity.this, MapActivity.class);
         startActivity(intent_map);
     }
 
     public void ClickFirstButton(View view){
-        Intent intent_first_button = new Intent(MainActivity.this, WhatSituation.class);
-        startActivity(intent_first_button);
+        this.ClickBtn(view);
     }
 
     public void ClickSecondButton(View view){
-        Intent intent_second_button = new Intent(MainActivity.this, SituationIsBad.class);
-        startActivity(intent_second_button);
+        this.ClickBtn(view);
     }
-
-    /*public void onClick(View view) {
-        Intent intent = new Intent(MainActivity.this, MapActivity.class);
-        startActivity(intent);
-    }*/
 }
--- app/src/main/java/com/example/dmitrii/testbtn/MapActivity.java
+++ app/src/main/java/com/example/dmitrii/testbtn/MapActivity.java
@@ -1,17 +1,28 @@
 package com.example.dmitrii.testbtn;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-import com.example.dmitrii.testbtn.R;
-
-/**
- * Created by dmitrii on 07.11.15.
- */
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends FragmentActivity  implements OnMapReadyCallback {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        LatLng fefu = new LatLng(43.026878, 131.892301);
+        map.addMarker(new MarkerOptions().position(fefu).title("Marker in FEFU"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(fefu));
     }
 }
